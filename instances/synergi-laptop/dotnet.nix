6ce74458e010301df3925251593a7d0e1d03e90f
Will Dixon <will@willd.io>
2023-09-27 02:41:51 -0400

    Add DOTNET_ROOT environment variable
    Signed-off-by: Will Dixon <will@willd.io>

--- instances/synergi-laptop/dotnet.nix
+++ instances/synergi-laptop/dotnet.nix
@@ -6,23 +6,23 @@ let
   cfg = config.profiles.synergi-laptop.dotnet;
   sdk_5_0 = (import ./derivations/dotnet-sdk-5-0.nix attrs);
   sdk_3_1 = (import ./derivations/dotnet-sdk-3-1.nix attrs);
+  allSDKs = with pkgs; (dotnetCorePackages.combinePackages [
+        dotnetCorePackages.sdk_7_0
+        dotnetCorePackages.sdk_6_0
+        sdk_5_0
+        sdk_3_1
+      ]);
 in {
   options.profiles.synergi-laptop.dotnet = {};
   imports = [
     ./derivations/az-cred-provider.nix
   ];
   config = {
-    home.packages = with pkgs; [
-      (dotnetCorePackages.combinePackages [
-        dotnetCorePackages.sdk_7_0
-        dotnetCorePackages.sdk_6_0
-        sdk_5_0
-        sdk_3_1
-      ])
-    ];
+    home.packages = [allSDKs];
     home.sessionVariables = {
       NUGET_PACKAGES = "${config.xdg.cacheHome}/NuGetPackages";
       OMNISHARPHOME = "${config.xdg.configHome}/omnisharp";
+      DOTNET_ROOT = "${allSDKs}";
     };
   };
 } 
