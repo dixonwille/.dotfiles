45beb582a79b586faae728fb964cce3bf4229991
Will Dixon <will@willd.io>
2024-01-20 15:29:50 -0500

    Accept the install
    Signed-off-by: Will Dixon <will@willd.io>

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -38,7 +38,7 @@ install_packages() {
   if [[ "${DFCONF[IS_FEDORA]}" == "1" ]];then
     sudo dnf update -y
     if [[ "${#_df_copr[@]}" -gt 0 ]];then
-      sudo dnf install dnf-plugins-core
+      sudo dnf install dnf-plugins-core -y
     fi
     for copr in "${_df_copr[@]}"; do
       sudo dnf copr enable "$copr" -y
