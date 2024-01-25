b4661c20f34e438f98b0ba7486c1baf3267499fd
Will Dixon <will@willd.io>
2024-01-24 22:07:37 -0500

    Install more tools work requires
    Signed-off-by: Will Dixon <will@willd.io>

--- modules/azure/module.sh
+++ modules/azure/module.sh
@@ -4,3 +4,7 @@ DFLOADED[AZURE]="1"
 load_module "microsoft"
 
 df_package "azure-cli"
+
+if [[ "${DFCONF[IS_WORK]}" == "1" ]]; then
+  df_after_install "az extension add --allow-preview false --name 'azure-devops'"
+fi
--- modules/powershell/module.sh
+++ modules/powershell/module.sh
@@ -4,3 +4,8 @@ DFLOADED[POWERSHELL]="1"
 load_module "microsoft"
 
 df_package "powershell"
+
+if [[ "${DFCONF[IS_WORK]}" == "1" ]]; then
+  df_after_install "pwsh -Command 'if (-not (Get-Module -ListAvailable -Name Az)) {Install-Module -Name Az -Repository PSGallery -Force}'"
+  df_after_install "pwsh -Command 'if (-not (Get-Module -ListAvailable -Name AzureAD)) {Install-Module -Name AzureAD -Repository PSGallery -Force}'"
+fi
