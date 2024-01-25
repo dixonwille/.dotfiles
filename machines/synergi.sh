4ea412aa1acc4597026d63d58059eb735f71ca4b
Will Dixon <will@willd.io>
2024-01-25 01:05:31 -0500

    Fix Dotnet installation
    Signed-off-by: Will Dixon <will@willd.io>

--- machines/synergi.sh
+++ machines/synergi.sh
@@ -7,7 +7,9 @@ df_module "dotnet"
 
 # dotnet_install available via dotnet module
 # vfox can only support down to 6.0 right now...
-df_after_install "dotnet_install 3.1"
-df_after_install "dotnet_install 5.0"
-df_after_install "dotnet_install 6.0"
-df_after_install "dotnet_install 7.0"
+df_before_install "dotnet_install 3.1"
+df_before_install "dotnet_install 5.0"
+df_before_install "dotnet_install 6.0"
+df_before_install "dotnet_install 7.0"
+
+df_package "jq"
