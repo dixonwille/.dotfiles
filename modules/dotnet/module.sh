1a23b96e054edaecf5f17331db34e57c632739a9
Will Dixon <will@willd.io>
2024-01-24 19:01:07 -0500

    Install dotnet correctly
    Signed-off-by: Will Dixon <will@willd.io>

--- modules/dotnet/module.sh
+++ modules/dotnet/module.sh
@@ -1,10 +1,27 @@
 [[ "${DFLOADED[DOTNET]}" == "1" ]] && return
 DFLOADED[DOTNET]="1"
 
-load_module "microsoft"
+dotnetDir="$XDG_DATA_HOME/dotnet"
+mkdir -p "$dotnetDir"
 
-df_package "dotnet-sdk-8.0"
-df_package "dotnet-sdk-7.0"
-df_package "dotnet-sdk-6.0"
+curl -sSL https://dot.net/v1/dotnet-install.sh > "$dotnetDir/dotnet-install.sh"
+chmod +x "$dotnetDir/dotnet-install.sh"
 
+dotnet_install() {
+  "$dotnetDir/dotnet-install.sh" --channel "$1" --install-dir "$dotnetDir"
+}
+
+df_package "krb5-libs"
+df_package "libicu"
+df_package "openssl-libs"
+df_package "zlib"
+
+df_after_install "dotnet_install 3.1"
+df_after_install "dotnet_install 5.0"
+df_after_install "dotnet_install 6.0"
+df_after_install "dotnet_install 7.0"
+df_after_install "dotnet_install 8.0"
+
+df_symlink "$dotnetDir/dotnet" "$HOME/.local/bin/dotnet"
+df_symlink "$dotnetDir/dotnet-install.sh" "$HOME/.local/bin/dotnet-install"
 # TODO: Install if work https://github.com/microsoft/artifacts-credprovider
