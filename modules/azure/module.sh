c9b0e7fc4866ca8de02d36c71d8d488af0802e65
Will Dixon <will@willd.io>
2024-01-31 10:10:06 -0500

    Add some more specific setting for work
    Signed-off-by: Will Dixon <will@willd.io>

--- modules/azure/module.sh
+++ modules/azure/module.sh
@@ -7,4 +7,6 @@ df_package "azure-cli"
 
 if [[ "${DFCONF[IS_WORK]}" == "1" ]]; then
   df_after_install "az extension add --allow-preview false --name 'azure-devops'"
+  df_after_install "az bicep install"
+  df_symlink "$HOME/.local/bin/bicep" "$XDG_DATA_HOME/azure/bin/bicep"
 fi
