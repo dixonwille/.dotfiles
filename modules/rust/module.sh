a19748f556293fa3519d025aa8db18bd125ae06e
Will Dixon <will@willd.io>
2024-02-06 21:11:49 -0500

    Add rustup and use system as default toolchain
    Signed-off-by: Will Dixon <will@willd.io>

--- modules/rust/module.sh
+++ modules/rust/module.sh
@@ -3,3 +3,7 @@ DFLOADED[RUST]="1"
 
 df_package "rust"
 df_package "cargo"
+source "$DOTFILESDIR/modules/rust/environment"
+df_after_install "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none --no-modify-path -y"
+df_after_install "rustup toolchain link system /usr"
+df_after_install "rustup default system"
