cb9a0142b70713f073fa78097e3e632655b6841a
Will Dixon <will@willd.io>
2024-03-14 18:22:37 -0400

    Add Github Copilot
    Signed-off-by: Will Dixon <will@willd.io>

---
+++ modules/neovim/rc/lua/base/codepilot.lua
@@ -0,0 +1,12 @@
+return {
+	"github/copilot.vim",
+	event = "InsertEnter",
+	cmd = {
+		"Copilot",
+	},
+	keys = { { "<C-a>", 'copilot#Accept("\\<CR>")', mode = "i", expr = true, replace_keycodes = false } },
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		require("copilot").setup()
+	end,
+}
