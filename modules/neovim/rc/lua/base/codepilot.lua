dc36feca8b74622832f7a9843be4428cfc3ffb58
Will Dixon <will@willd.io>
2024-03-14 23:23:48 -0400

    No need to setup Copilot
    Signed-off-by: Will Dixon <will@willd.io>

--- modules/neovim/rc/lua/base/codepilot.lua
+++ modules/neovim/rc/lua/base/codepilot.lua
@@ -7,6 +7,5 @@ return {
 	keys = { { "<C-a>", 'copilot#Accept("\\<CR>")', mode = "i", expr = true, replace_keycodes = false } },
 	config = function()
 		vim.g.copilot_no_tab_map = true
-		require("copilot").setup()
 	end,
 }
