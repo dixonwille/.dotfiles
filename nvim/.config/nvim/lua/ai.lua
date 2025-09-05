acb59f461858d45d2d4f217964129c42eedc56df
Will Dixon <will@willd.io>
2025-09-05 14:02:44 -0400

    Setup basic MCPHub

--- nvim/.config/nvim/lua/ai.lua
+++ nvim/.config/nvim/lua/ai.lua
@@ -10,6 +10,13 @@ require("copilot").setup({
   }
 })
 
+require("mcphub").setup({
+  auto_approve = true,
+  global_env = {
+    PROJECT_ROOT = vim.fn.getcwd()
+  }
+})
+
 vim.g.codecompanion_auto_tool_mode = true -- Automatically allow all tools (can be dangerous but use a version control)
 require("codecompanion").setup({
   strategies = {
@@ -32,6 +39,19 @@ require("codecompanion").setup({
       }
     },
   },
+  extensions = {
+    mcphub = {
+      callback = "mcphub.extensions.codecompanion",
+      opts = {
+        make_tools = true,
+        show_server_tools_in_chat = true,
+        add_mcp_prefix_to_tool_names = false,
+        show_result_in_chat = true,
+        make_vars = true,
+        make_slash_commands = true,
+      }
+    }
+  }
 })
 
 vim.keymap.set({ "n" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "Open Chat" })
