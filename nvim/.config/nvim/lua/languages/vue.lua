0e9019eb77af7984dfb68643603d691442eb7d27
Will Dixon <will@willd.io>
2025-09-23 20:25:45 -0400

    Add Vue support to neovim
    Also don't show Hyperland updates message

---
+++ nvim/.config/nvim/lua/languages/vue.lua
@@ -0,0 +1,25 @@
+-- TODO (wd): include treesitter
+-- TODO (wd): include mason
+
+local vue_language_server_path = vim.fn.expand('$MASON/packages/vue-language-server/node_modules/@vue/language-server')
+local vue_plugin = {
+  name = '@vue/typescript-plugin',
+  location = vue_language_server_path,
+  languages = { 'vue' },
+  configNamespace = 'typescript'
+}
+vim.lsp.config('vtsls', {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin
+        }
+      }
+    }
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+})
+
+vim.lsp.enable("vtsls")
+vim.lsp.enable("vue_ls")
